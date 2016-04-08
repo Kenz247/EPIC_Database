852a816a0b2ca15f7502dcba040e890879067f8a
Kenz247 <kenzo.wong247@gmail.com>
2016-04-08 17:11:37 -0400

    Pushing

--- interest.php
+++ interest.php
@@ -7,31 +7,31 @@
     <h1>Find a project for you!</h1>
     <div align="center">
       <label class="radio-inline"><input type="radio" name="optradio">Medicie</label>
-      <label class="radio-inline"><input type="radio" name="optradio">Artifical Intelligence</label>
+      <label class="radio-inline"><input type="radio" name="optradio1">Artifical Intelligence</label>
     </div>
     <div align="center">
-      <label class="radio-inline"><input type="radio" name="optradio">App Development</label>
-      <label class="radio-inline"><input type="radio" name="optradio">Web Development</label>
+      <label class="radio-inline"><input type="radio" name="optradio2">App Development</label>
+      <label class="radio-inline"><input type="radio" name="optradio3">Web Development</label>
     </div>
     <div>
-      <label class="radio-inline"><input type="radio" name="optradio">Art</label>
-      <label class="radio-inline"><input type="radio" name="optradio">Music</label>
+      <label class="radio-inline"><input type="radio" name="optradio4">Art</label>
+      <label class="radio-inline"><input type="radio" name="optradio5">Music</label>
     </div>
     <div>
-      <label class="radio-inline"><input type="radio" name="optradio">Photography</label>
-      <label class="radio-inline"><input type="radio" name="optradio">Sports</label>
+      <label class="radio-inline"><input type="radio" name="optradio6">Photography</label>
+      <label class="radio-inline"><input type="radio" name="optradio7">Sports</label>
     </div>
     <div>
-      <label class="radio-inline"><input type="radio" name="optradio">Media</label>
-      <label class="radio-inline"><input type="radio" name="optradio">History</label>
+      <label class="radio-inline"><input type="radio" name="optradio8">Media</label>
+      <label class="radio-inline"><input type="radio" name="optradio9">History</label>
     </div>
     <div>
-      <label class="radio-inline"><input type="radio" name="optradio">Math</label>
+      <label class="radio-inline"><input type="radio" name="optradio9">Math</label>
       <label class="radio-inline"><input type="radio" name="optradio">Theater</label>
     </div>
     <div>
-      <label class="radio-inline"><input type="radio" name="optradio">International</label>
-      <label class="radio-inline"><input type="radio" name="optradio">Politics</label>
+      <label class="radio-inline"><input type="radio" name="optradio10">International</label>
+      <label class="radio-inline"><input type="radio" name="optradio11">Politics</label>
     </div>
     <div>
       <label class="radio-inline"><input type="radio" name="optradio">Social Work</label>
